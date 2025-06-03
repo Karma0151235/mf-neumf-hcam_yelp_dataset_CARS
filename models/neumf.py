8f01e898f2f24012dbe1009883eb3ad1c44b6517
Stephen Tee <stephentee88@gmail.com>
2025-06-04 01:05:16 +0800

    Update neumf.py

--- models/neumf.py
+++ models/neumf.py
@@ -10,7 +10,6 @@ class GMF(nn.Module):
         self.embedding_user = nn.Embedding(num_users, latent_dim)
         self.embedding_item = nn.Embedding(num_items, latent_dim)
         self.affine_output = nn.Linear(latent_dim, 1)
-        self.logistic = nn.Sigmoid()
 
         self._init_weights(init_std)
 
@@ -19,7 +18,7 @@ class GMF(nn.Module):
         i = self.embedding_item(item_indices)
         x = torch.mul(u, i)
         x = self.affine_output(x)
-        return self.logistic(x)
+        return x  # Raw output for regression
 
     def _init_weights(self, std):
         nn.init.normal_(self.embedding_user.weight, std=std)
@@ -29,7 +28,7 @@ class GMF(nn.Module):
 
 
 class MLP(nn.Module):
-    def __init__(self, num_users, num_items, layers=[64, 32, 16, 8], init_std=0.01):
+    def __init__(self, num_users, num_items, layers=[64, 32, 16, 8], dropout=0.2, init_std=0.01):
         super(MLP, self).__init__()
         assert layers[0] % 2 == 0, "First layer size should be divisible by 2."
 
@@ -37,11 +36,11 @@ class MLP(nn.Module):
         self.embedding_item = nn.Embedding(num_items, layers[0] // 2)
 
         self.fc_layers = nn.ModuleList()
+        self.dropout = nn.Dropout(p=dropout)
         for in_size, out_size in zip(layers[:-1], layers[1:]):
             self.fc_layers.append(nn.Linear(in_size, out_size))
 
         self.affine_output = nn.Linear(layers[-1], 1)
-        self.logistic = nn.Sigmoid()
 
         self._init_weights(init_std)
 
@@ -51,8 +50,9 @@ class MLP(nn.Module):
         x = torch.cat([u, i], dim=-1)
         for layer in self.fc_layers:
             x = torch.relu(layer(x))
+            x = self.dropout(x)
         x = self.affine_output(x)
-        return self.logistic(x)
+        return x
 
     def _init_weights(self, std):
         nn.init.normal_(self.embedding_user.weight, std=std)
@@ -65,7 +65,7 @@ class MLP(nn.Module):
 
 
 class NeuMF(nn.Module):
-    def __init__(self, num_users, num_items, latent_dim_mf=16, latent_dim_mlp=32, mlp_layers=[64, 32, 16, 8], init_std=0.01):
+    def __init__(self, num_users, num_items, latent_dim_mf=16, latent_dim_mlp=32, mlp_layers=[64, 32, 16, 8], dropout=0.2, init_std=0.01):
         super(NeuMF, self).__init__()
         self.embedding_user_mlp = nn.Embedding(num_users, latent_dim_mlp)
         self.embedding_item_mlp = nn.Embedding(num_items, latent_dim_mlp)
@@ -73,12 +73,12 @@ class NeuMF(nn.Module):
         self.embedding_item_mf = nn.Embedding(num_items, latent_dim_mf)
 
         self.fc_layers = nn.ModuleList()
+        self.dropout = nn.Dropout(p=dropout)
         for in_size, out_size in zip(mlp_layers[:-1], mlp_layers[1:]):
             self.fc_layers.append(nn.Linear(in_size, out_size))
 
         predict_size = mlp_layers[-1] + latent_dim_mf
         self.affine_output = nn.Linear(predict_size, 1)
-        self.logistic = nn.Sigmoid()
 
         self._init_weights(init_std)
 
@@ -91,11 +91,12 @@ class NeuMF(nn.Module):
         x_mlp = torch.cat([u_mlp, i_mlp], dim=-1)
         for layer in self.fc_layers:
             x_mlp = torch.relu(layer(x_mlp))
+            x_mlp = self.dropout(x_mlp)
 
         x_mf = torch.mul(u_mf, i_mf)
         x = torch.cat([x_mlp, x_mf], dim=-1)
         x = self.affine_output(x)
-        return self.logistic(x)
+        return x
 
     def _init_weights(self, std):
         for emb in [self.embedding_user_mlp, self.embedding_item_mlp, self.embedding_user_mf, self.embedding_item_mf]:
